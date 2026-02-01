7014b4e52e864c5c06fe78763e8b75909947ec4e
Khushi Aggarwal <aggarwalkhushi073@gmail.com>
2026-02-01 22:41:11 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT = "-e ."
+
+def get_requirements(file_path: str) -> List[str]:
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.read().splitlines()
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+setup(
+    name="mlproject",
+    version="0.0.1",
+    author="khushi",
+    author_email="aggrawalkhushi076@gmail.com",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=get_requirements("requirements.txt"),
+)
